dd65dbc3400bd548b2db00e58228c590743f162d
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-26 22:29:39 -0300

    feat: header home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     RouterModule,
