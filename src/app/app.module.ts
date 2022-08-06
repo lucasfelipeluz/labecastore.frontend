d6d3e715b65539f8a84b39bcc60a57787d1a09de
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-08-05 23:12:06 -0300

    chore: ajustes app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,18 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt'
+import localePt from '@angular/common/locales/pt';
 
 import { AppRouting } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { AuthAdminService } from './guards/auth-admin.service';
 
-registerLocaleData(localePt, 'pt')
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-
-    AppRouting,
-
-    HomeModule
-  ],
-  providers: [
-    {provide: LOCALE_ID, useValue: 'pt'},
-    AuthAdminService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule, AppRouting],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
