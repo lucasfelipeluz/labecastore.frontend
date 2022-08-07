31c2c4fb93f4ba154aa57794909e8a1edc7b62bb
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-08-07 14:28:40 -0300

    feat: ajustes no enviroment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import localePt from '@angular/common/locales/pt';
 import { AppRouting } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './responses/not-found/not-found.component';
+import { UnavailableComponent } from './responses/unavailable/unavailable.component';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent, UnavailableComponent],
   imports: [BrowserModule, RouterModule, AppRouting],
   providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public/public.component';
 
-const app_routes: Routes = [
-  {
+import { environment } from 'src/environments/environment';
+import { NotFoundComponent } from './responses/not-found/not-found.component';
+import { UnavailableComponent } from './responses/unavailable/unavailable.component';
+
+const app_routes: Routes = [];
+
+if (environment.run === true) {
+  app_routes.push({
     path: '',
     loadChildren: () =>
       import('./public/public.module').then((m) => m.PublicModule),
-  },
-];
+  });
+}
+
+if (environment.run === false) {
+  app_routes.push({
+    path: '',
+    component: UnavailableComponent,
+  });
+}
+
+app_routes.push({ path: '**', component: NotFoundComponent });
 
 @NgModule({
   imports: [RouterModule.forRoot(app_routes)],
